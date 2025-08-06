bb6f85f3ae6dbdc057e5b6bdf4d884dc6aa650d7
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-08-06 13:10:00 +0200

    create rfc component

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,10 @@
-import Form from '@rjsf/core';
-import validator from '@rjsf/validator-ajv8';
+import Rsf from "./components/Rsf";
 
-const schema = {
-  title: 'Test form',
-  type: 'object',
-  properties: {
-    name: {
-      type: 'string',
-    },
-    age: {
-      type: 'number',
-    },
-  },
-};
-
-const uiSchema = {
-  name: {
-    'ui:classNames': 'p-4',
-  },
-  age: {
-    'ui:classNames': 'custom-class-age',
-  },
-};
-
-const formData = {
-  name: 'John Doe',
-  age: 30,
-};
-
-const onSubmit = ({ formData }, e) => console.log('Data submitted: ', formData);
 
 function App() {
 
   return (
-    <div className="w-full p-4">
-        <h1 className="text-3xl text-center">Belis App</h1>
-        <Form schema={schema} validator={validator} onSubmit={onSubmit} uiSchema={uiSchema} formData={formData} />
-    </div>
+    <Rsf />
   );
 }
 
