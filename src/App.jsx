83af220a61fc47f7bf600762a9f1569249440178
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-08-06 09:42:17 +0200

    install rjsf form library

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
-import { Button, DatePicker, Space, version } from "antd";
-import { useState } from "react";
-import useOnUrlChange from "./hooks/useOnUrlChange";
-import { useSelector, useDispatch } from "react-redux";
-import { getData, setData } from "./store/slices/dataSlice";
+import Form from '@rjsf/core';
+import validator from '@rjsf/validator-ajv8';
+
+const schema = {
+  title: 'Test form',
+  type: 'string',
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-  const getDates = useSelector(getData);
-  const dispatch = useDispatch();
-  const param = useOnUrlChange("test");
+
   return (
-    <div className="w-full">
-      <div className="ml-8 text-3xl font-bold underline">Hello world!</div>
-      <div style={{ padding: "0 24px" }}>
-        <h1>antd version: {param ? param : version}</h1>
-        <Space>
-          <DatePicker />
-          <Button type="primary">Primary Button</Button>
-        </Space>
-        <div
-          onClick={() => {
-            dispatch(setData(["banana", "hvost"]));
-          }}
-        >
-          Set
-        </div>
-      </div>
+    <div className="w-full p-4">
+        <h1 className="text-3xl text-center">Belis App</h1>
+        <Form schema={schema} validator={validator} />
     </div>
   );
 }
