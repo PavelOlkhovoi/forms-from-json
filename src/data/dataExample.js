1d2fc5e5c90e38929e97fcc973e16ab411d46648
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-08-07 16:02:13 +0200

    add mast type data example

--- src/data/dataExample.js
+++ src/data/dataExample.js
@@ -253,3 +253,677 @@ export const infobaustein_template =
     ]
   }
 }
+
+
+export masstype = {
+  "data": {
+    "tkey_masttyp": [
+      {
+        "wandstaerke": null,
+        "masttyp": "B25",
+        "lph": null,
+        "id": 24,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "SS",
+        "lph": null,
+        "id": 79,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "TM",
+        "lph": null,
+        "id": 80,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "VM3",
+        "lph": null,
+        "id": 81,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "VM31",
+        "lph": null,
+        "id": 82,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "VM32",
+        "lph": null,
+        "id": 83,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "VM4",
+        "lph": null,
+        "id": 84,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "VM5",
+        "lph": null,
+        "id": 85,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "VM6",
+        "lph": null,
+        "id": 86,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "VM7",
+        "lph": null,
+        "id": 87,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "VM8",
+        "lph": null,
+        "id": 88,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "VM9",
+        "lph": null,
+        "id": 89,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": null,
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M11",
+        "lph": 9,
+        "id": 28,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "Doppel-Auslegermast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 3,
+            "tkey_masttyp": 28,
+            "dms_url": {
+              "id": 9,
+              "description": "M11.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 9,
+                "object_name": "DOC-1396266265055-1067868849.pdf",
+                "url_base": {
+                  "id": 9,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M12",
+        "lph": 10,
+        "id": 30,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "Doppel-Auslegermast ",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 4,
+            "tkey_masttyp": 30,
+            "dms_url": {
+              "id": 10,
+              "description": "M12.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 10,
+                "object_name": "DOC-1396266859665-1068792370.pdf",
+                "url_base": {
+                  "id": 10,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": 3,
+        "masttyp": "M57",
+        "lph": 7,
+        "id": 65,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "7m Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": 3,
+        "masttyp": "M56",
+        "lph": 6,
+        "id": 64,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "6m Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 296,
+            "tkey_masttyp": 64,
+            "dms_url": {
+              "id": 62,
+              "description": "M5.6.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 62,
+                "object_name": "DOC-1401807984959-2081210540.pdf",
+                "url_base": {
+                  "id": 62,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": 3,
+        "masttyp": "M53",
+        "lph": 8,
+        "id": 63,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "8m Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 297,
+            "tkey_masttyp": 63,
+            "dms_url": {
+              "id": 63,
+              "description": "M5.3.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 63,
+                "object_name": "DOC-1401808056220-2083981103.pdf",
+                "url_base": {
+                  "id": 63,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M4",
+        "lph": null,
+        "id": 55,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "Ausleger",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 299,
+            "tkey_masttyp": 55,
+            "dms_url": {
+              "id": 65,
+              "description": "M4.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 65,
+                "object_name": "DOC-1401808244197-2041018357.pdf",
+                "url_base": {
+                  "id": 65,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M1",
+        "lph": 9,
+        "id": 25,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "9m Auslegermast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 15,
+            "tkey_masttyp": 25,
+            "dms_url": {
+              "id": 7,
+              "description": "M1.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 7,
+                "object_name": "DOC-1396265284955-2043788920.pdf",
+                "url_base": {
+                  "id": 7,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M5",
+        "lph": 7.5,
+        "id": 59,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "7,5m Auslegermast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 39,
+            "tkey_masttyp": 59,
+            "dms_url": {
+              "id": 19,
+              "description": "M5.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 19,
+                "object_name": "DOC-1396268104258-2040094836.pdf",
+                "url_base": {
+                  "id": 19,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M14",
+        "lph": 4,
+        "id": 33,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "4m Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 294,
+            "tkey_masttyp": 33,
+            "dms_url": {
+              "id": 13,
+              "description": "M14.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 13,
+                "object_name": "DOC-1396267579506-1070639412.pdf",
+                "url_base": {
+                  "id": 13,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M3",
+        "lph": 5,
+        "id": 42,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "5m Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 199,
+            "tkey_masttyp": 42,
+            "dms_url": {
+              "id": 17,
+              "description": "M3.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 17,
+                "object_name": "DOC-1396267880608-2041941878.pdf",
+                "url_base": {
+                  "id": 17,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M31",
+        "lph": 5,
+        "id": 44,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "5m Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 127,
+            "tkey_masttyp": 44,
+            "dms_url": {
+              "id": 18,
+              "description": "M3.1.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 18,
+                "object_name": "DOC-1396267952918-434131789.pdf",
+                "url_base": {
+                  "id": 18,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M2",
+        "lph": 10,
+        "id": 40,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "10m Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 295,
+            "tkey_masttyp": 40,
+            "dms_url": {
+              "id": 8,
+              "description": "M2.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 8,
+                "object_name": "DOC-1396265374344-2042865399.pdf",
+                "url_base": {
+                  "id": 8,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M17",
+        "lph": 4.6,
+        "id": 38,
+        "hersteller": "Fa. König",
+        "foto": null,
+        "bezeichnung": "V2A  Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 267,
+            "tkey_masttyp": 38,
+            "dms_url": {
+              "id": 16,
+              "description": "M17.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 16,
+                "object_name": "DOC-1396267838265-1073409975.pdf",
+                "url_base": {
+                  "id": 16,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M13",
+        "lph": 12,
+        "id": 31,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "12m Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 5,
+            "tkey_masttyp": 31,
+            "dms_url": {
+              "id": 11,
+              "description": "M13.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 11,
+                "object_name": "DOC-1396266917679-1069715891.pdf",
+                "url_base": {
+                  "id": 11,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": 4,
+        "masttyp": "M13.1",
+        "lph": 12,
+        "id": 32,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "12m Aufsatzmast ZM 108",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 6,
+            "tkey_masttyp": 32,
+            "dms_url": {
+              "id": 12,
+              "description": "M13.1.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 12,
+                "object_name": "DOC-1396267091253-1440009782.pdf",
+                "url_base": {
+                  "id": 12,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "M16",
+        "lph": 6.5,
+        "id": 35,
+        "hersteller": "Bega",
+        "foto": null,
+        "bezeichnung": "6,5m Aufsatzmast",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 9,
+            "tkey_masttyp": 35,
+            "dms_url": {
+              "id": 15,
+              "description": "M16.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 15,
+                "object_name": "DOC-1396267697722-1072486454.pdf",
+                "url_base": {
+                  "id": 15,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "S",
+        "lph": null,
+        "id": 96,
+        "hersteller": null,
+        "foto": null,
+        "bezeichnung": "Strahler",
+        "dms_url": null,
+        "dokumenteArray": []
+      },
+      {
+        "wandstaerke": null,
+        "masttyp": "216",
+        "lph": 3.5,
+        "id": 16,
+        "hersteller": "Trapp     f-trapp.de",
+        "foto": null,
+        "bezeichnung": "Kandellaber Modell \"Düsseldorf\" Nr. 640",
+        "dms_url": null,
+        "dokumenteArray": [
+          {
+            "id": 370,
+            "tkey_masttyp": 16,
+            "dms_url": {
+              "id": 120,
+              "description": "THK640_DUESSELDORF.pdf",
+              "name": null,
+              "typ": null,
+              "url": {
+                "id": 120,
+                "object_name": "DOC-1415195714104-240113766.pdf",
+                "url_base": {
+                  "id": 120,
+                  "path": "/belis/",
+                  "prot_prefix": "http://",
+                  "server": "board.cismet.de"
+                }
+              }
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
